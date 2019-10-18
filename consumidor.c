c9ad04bda2be50b6fef7083668f3a66affc3b03a
Cleberton-Oliveira <clebertonoliveira.cnt@gmail.com>
2019-10-17 21:29:52 -0300

    Criando o struct

--- consumidor.c
+++ consumidor.c
@@ -1,21 +1,33 @@
 // Trabalo SO Consumidor de Musica 
+#include <pthread.h> 
 #include <stdio.h> 
+#include <unistd.h>
 
 #define NUM_REPETICOES  30
 
 pthread_t reprodutor; 
-int musica;
+int musica = 0;
 
 void *reproduzMusica(){  
   for (long i = 0; i < NUM_REPETICOES; i++){
     // espera 5 segundos 
+  	unsigned sleep(500);
 	//traca o sistema com semaforo 
+
 	// reproduz a musica
-	// imprime a musica que preoduziu 
+
+	// imprime a musica que preoduziu
+	printf("Está escutando a musica: %s\n")
+	printf("Nome da Musica: %s\n", nomeMusica);
+	printf("Autor da Musica: %s\n", autorMusica);
+	printf("Genero da Musuca %s\n", generoMusica);
+	printf("Tempo de duração: %s\n", duracao);
 	// musica++
+	musica++;
 	// destranca o sistema 
   }
   printf("Já reproduziu a musica\n");
+
 }
 
 
--- produtor.c
+++ produtor.c
@@ -1,18 +1,40 @@
 // Trabalo SO Produtor de Musica 
-
 #include <pthread.h> 
 #include <stdio.h> 
 
 #define NUM_REPETICOES  10
 
 pthread_t prod0, prod1, prod2;  
-int novaMusica;
+int novaMusica = 0;
+
+
+typedef struct { 
+    char nomeMusica[30]; 
+    char autorMusica[30];   
+    char generoMusica[30];
+    char duracao[30]; 
+} Musica;
+
+Musica SetMusica(char nomeMusica, char autorMusica, char generoMusica, char duracao) 
+{  
+    char musica = pessoa + novaMusica;
+    Musica musica;
+    musica.nomeMusica = nomeMusica;  
+    musica.autorMusica = autorMusica;  
+    musica.generoMusica = generoMusica;  
+    musica.duracao = duracao;  
+
+    novaMusica++;
+    return musica; // retorna a struct contendo os dados passados por parâmetro
+}
+
 
 void *criaMusica(){  
   for (long i = 0; i < NUM_REPETICOES; i++){
     // espera 5 segundos 
 	//traca o sistema com semaforo 
 	// cria nova musica 
+  	SetMusica('Dogs', 'Pink Floyd', 'Rock', '3:14');
 	// novaMusica++;
 	// destranca o sistema 
   }
